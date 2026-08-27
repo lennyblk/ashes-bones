e1e72d4f6141a3cc9fad66e0283aa5208b01824d
lennyblk <len06blackett@gmail.com>
2026-08-27 15:35:04 +0200

    animation + assets

---
+++ assets/map/readme.txt
@@ -0,0 +1,23 @@
+Thank you for purchasing my asset pack! 
+
+If you encounter any issues or have questions, contact me on social media, leave a comment on itch, or write an e-mail (see below).
+
+License
+
+You can use the assets in commercial and non-commercial projects.
+You can also modify the assets to your liking and use them in commercial and non-commercial projects.
+You are not allowed to resell the assets, even if they are modified.
+
+You don't have to credit me in your projects, although it is appreciated.
+
+I hope you have fun with your project!
+
+- Grajo
+
+
+Socials
+
+Twitter: https://x.com/grajopx
+Email: grajopx@protonmail.com
+itch: https://grajopx.itch.io
+
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,45 @@
-use raylib::consts::KeyboardKey;
 use raylib::prelude::*;
 
 const SCREEN_WIDTH: i32 = 800;
 const SCREEN_HEIGHT: i32 = 600;
+
+struct Animation {
+    texture: Texture2D,
+    frame_width: i32,
+    frame_height: i32,
+    frames_per_row: i32,
+    first: i32,
+    last: i32,
+    current: i32,
+    speed: f32,
+    duration_left: f32,
+}
+
+impl Animation {
+    fn animation_update(&mut self, delta_time: f32) {
+        self.duration_left -= delta_time;
+
+        if self.duration_left <= 0.0 {
+            self.current += 1;
+            if self.current > self.last {
+                self.current = self.first;
+            }
+            self.duration_left = 1.0 / self.speed;
+        }
+    }
+
+    fn animation_frame(&self) -> Rectangle {
+        let col = self.current % self.frames_per_row;
+        let row = self.current / self.frames_per_row;
+
+        Rectangle {
+            x: (col * self.frame_width) as f32,
+            y: (row * self.frame_height) as f32,
+            width: self.frame_width as f32,
+            height: self.frame_height as f32,
+        }
+    }
+}
 
 fn main() {
     // init window
@@ -11,8 +48,32 @@ fn main() {
         .title("Ashes&Bones")
         .build();
 
+    //load texture
+    let human_idle_texture = rl
+        .load_texture(
+            &thread,
+            "assets/humanChar/Human soldier/Human soldier/human_soldier-Idle.png",
+        )
+        .unwrap();
+
+    let mut animation = Animation {
+        texture: human_idle_texture,
+        frame_width: 130,
+        frame_height: 100,
+        frames_per_row: 7,
+        first: 0,
+        last: 6,
+        current: 0,
+        speed: 8.0,
+        duration_left: 0.1,
+    };
+
     // run window
     while !rl.window_should_close() {
+        let delta_time = rl.get_frame_time();
+
+        animation.animation_update(delta_time);
+
         // drawing
         let mut d = rl.begin_drawing(&thread);
         d.clear_background(Color::RAYWHITE);
@@ -24,5 +85,19 @@ fn main() {
         for i in (40..SCREEN_WIDTH).step_by(40) {
             d.draw_rectangle_lines(i, 0, 1, SCREEN_HEIGHT, Color::BLACK);
         }
+
+        d.draw_texture_pro(
+            &animation.texture,
+            animation.animation_frame(),
+            Rectangle {
+                x: -40.0 as f32,
+                y: -40.0 as f32,
+                width: 128.0,
+                height: 128.0,
+            },
+            Vector2::new(0.0, 0.0),
+            0.0,
+            Color::WHITE,
+        );
     }
 }
