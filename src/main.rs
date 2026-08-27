4c869cfa94eaa2b484ff1ed6289a6b2bacf5981d
lennyblk <len06blackett@gmail.com>
2026-08-27 13:30:33 +0200

    grid

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use raylib::consts::KeyboardKey;
+use raylib::prelude::*;
+
+const SCREEN_WIDTH: i32 = 800;
+const SCREEN_HEIGHT: i32 = 600;
+
 fn main() {
-    println!("Hello, world!");
+    // init window
+    let (mut rl, thread) = raylib::init()
+        .size(SCREEN_WIDTH, SCREEN_HEIGHT)
+        .title("Ashes&Bones")
+        .build();
+
+    // run window
+    while !rl.window_should_close() {
+        // drawing
+        let mut d = rl.begin_drawing(&thread);
+        d.clear_background(Color::RAYWHITE);
+
+        for i in (40..SCREEN_HEIGHT).step_by(40) {
+            d.draw_rectangle_lines(0, i, SCREEN_WIDTH, 1, Color::BLACK);
+        }
+
+        for i in (40..SCREEN_WIDTH).step_by(40) {
+            d.draw_rectangle_lines(i, 0, 1, SCREEN_HEIGHT, Color::BLACK);
+        }
+    }
 }
